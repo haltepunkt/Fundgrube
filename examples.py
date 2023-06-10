a0a9d06711f4687ccfb4829fa321ecced4fe7403
Haltepunkt <33886909+haltepunkt@users.noreply.github.com>
2023-06-10 16:52:42 +0200

    Add comments to examples

--- examples.py
+++ examples.py
@@ -5,20 +5,25 @@ pp = pprint.PrettyPrinter()
 
 fundgrube = Fundgrube(retailer=Retailer.MEDIAMARKT)
 
+# Categories, including counts of postings
 pp.pprint(fundgrube.categories())
 
+# Brands, including counts of postings
 pp.pprint(fundgrube.brands()[:4])
 
-outlets = fundgrube.outlets()
-pp.pprint(outlets[:4])
+# Outlets, including counts of postings
+pp.pprint(fundgrube.outlets()[:4])
 
+# Postings by category
 postings, _, _ = fundgrube.postings(limit=4, category_ids=['CAT_DE_MM_115', 'CAT_DE_MM_8000'])
 pp.pprint(postings)
 
+# Postings by brand
 postings, _, url = fundgrube.postings(limit=4, brands=['APPLE', 'CASIO'])
 pp.pprint(url)
 pp.pprint(postings)
 
+# Outlet by name, followed by postings by outlet
 outlet = fundgrube.outlet(outlet_name='Bonn')
 
 if outlet is not None:
@@ -27,5 +32,6 @@ if outlet is not None:
   postings, _, _ = fundgrube.postings(limit=4, outlet_ids=[outlet])
   pp.pprint(postings)
 
+# Text search
 postings, _, _ = fundgrube.postings(limit=4, search='iPhone SE')
 pp.pprint(postings)
